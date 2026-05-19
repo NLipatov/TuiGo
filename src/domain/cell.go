09a5f76b368592382aa6895608072d35edbbbf07
Nikita Lipatov <6stringsohei@gmail.com>
2026-05-19 17:23:43 +0400

    feat: add new Cell methods: Foreground, Background, Symbol

--- src/domain/cell.go
+++ src/domain/cell.go
@@ -4,3 +4,23 @@ type Cell struct {
 	fg, bg Color
 	symbol rune
 }
+
+func NewCell(fg, bg Color, symbol rune) Cell {
+	return Cell{
+		fg:     fg,
+		bg:     bg,
+		symbol: symbol,
+	}
+}
+
+func (c *Cell) Foreground() Color {
+	return c.fg
+}
+
+func (c *Cell) Background() Color {
+	return c.bg
+}
+
+func (c *Cell) Symbol() rune {
+	return c.symbol
+}
