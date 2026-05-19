ed74386b71b524c403360fa503dfae28b770fd56
Nikita Lipatov <6stringsohei@gmail.com>
2026-05-19 17:24:17 +0400

    refactor: value receivers methods on Cell

--- src/domain/cell.go
+++ src/domain/cell.go
@@ -5,22 +5,22 @@ type Cell struct {
 	symbol rune
 }
 
-func NewCell(fg, bg Color, symbol rune) Cell {
+func NewCell(symbol rune, fg, bg Color) Cell {
 	return Cell{
+		symbol: symbol,
 		fg:     fg,
 		bg:     bg,
-		symbol: symbol,
 	}
 }
 
-func (c *Cell) Foreground() Color {
+func (c Cell) Foreground() Color {
 	return c.fg
 }
 
-func (c *Cell) Background() Color {
+func (c Cell) Background() Color {
 	return c.bg
 }
 
-func (c *Cell) Symbol() rune {
+func (c Cell) Symbol() rune {
 	return c.symbol
 }
